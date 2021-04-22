a2a2b781c2e21d9f6255e180ea8794e2e7fe126a
girkarkshitij <girkarkshitij@gmail.com>
2021-04-22 11:31:36 +0530

    Remove emojis from aboutMe section

--- src/data.js
+++ src/data.js
@@ -24,7 +24,7 @@ const data = {
     },
   ],
   aboutMe:
-    "I'm a third year student pursuing Bachelors's in Computer Engineering 🎓 from Mumbai University 🏛. I'm a passionate learner who's always willing to learn and work across technologies and domains 💡. I love to explore new technologies ✨. 💻 I'm currently into Web Development 🕸️",
+    "I'm a third year student pursuing Bachelors's in Computer Engineering from Mumbai University. I'm a passionate learner who's always willing to learn and work across technologies and domains. I love to explore new technologies.",
   address: 'Mumbai, India',
   website: 'https://kshitijgirkar.me',
   skills: [
