a66c740b761738fe3b62b0e297d85b3ab6614fa1
girkarkshitij <girkarkshitij@gmail.com>
2021-04-23 11:06:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Containers/Header';
+import Navbar from './components/nav/Navbar';
+import Header from './components/containers/header/Header';
+import AboutMe from './components/containers/aboutme/AboutMe';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <AboutMe />
     </div>
   );
 };
