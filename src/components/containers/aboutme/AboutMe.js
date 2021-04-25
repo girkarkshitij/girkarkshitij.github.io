e7f50104f1934f455c1fe0c915b07b2126eaee0e
girkarkshitij <girkarkshitij@gmail.com>
2021-04-25 19:16:10 +0530

    Make aboutme section responsive

--- src/components/containers/aboutme/AboutMe.js
+++ src/components/containers/aboutme/AboutMe.js
@@ -6,14 +6,15 @@ const AboutMe = () => {
   const { skills } = data;
 
   return (
-    <div id='about' className=' container text-light bg-dark px-32 pb-32 pt-24'>
-      <div className='flex divide-x divide-light2'>
-        <div className=''>
-          <h1 className='text-3xl h-2/4 text-light text-center pt-20 pr-8'>
-            About Me
-          </h1>
-        </div>
-        <div className='p-8 text-lg text-medium2'>
+    <div
+      id='about'
+      className='container text-light bg-medium md:px-32 md:pb-32 md:pt-24'
+    >
+      <div className='md:flex md:divide-x md:divide-light2'>
+        <h1 className='text-3xl h-2/4 text-light text-center pt-20 md:pr-8'>
+          About Me
+        </h1>
+        <div className='p-8 text-base md:text-xl text-medium2'>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam
             pariatur eaque placeat! Explicabo nulla, animi harum blanditiis
@@ -33,7 +34,7 @@ const AboutMe = () => {
           </p>
         </div>
       </div>
-      <div className='text-center text-lg p-12 pt-20'>
+      <div className='text-center text-xl pt-12 md:text-lg md:p-12 md:pt-20'>
         Technologies I'm working with
         <br />
         {skills.map((item) => {
--- src/components/containers/aboutme/SkillItem.js
+++ src/components/containers/aboutme/SkillItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SkillItem = ({ skill }) => {
-  return <span className='text-light2 p-8 inline-block'>{skill}</span>;
+  return <span className='text-light2 p-2 lg:p-8 inline-block'>{skill}</span>;
 };
 
 export default SkillItem;
