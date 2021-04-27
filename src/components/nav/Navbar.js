5fff3f94927e888897bcd151b270cac3001373fe
girkarkshitij <girkarkshitij@gmail.com>
2021-04-27 11:48:46 +0530

    Update favicon and add logo on navbar

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 
+import logo from '../../assets/KshitijGirkar-logo.png';
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -10,7 +12,10 @@ const Navbar = () => {
 
   return (
     <nav className='bg-dark sticky top-0 text-light pr-2 pb-2 sm:pb-4'>
-      <div className='flex justify-end'>
+      <div className='flex justify-between lg:hidden'>
+        <div>
+          <img src={logo} alt='logo' className='h-12 w-12 ml-8 mt-2' />
+        </div>
         <button
           className='focus:outline-none block p-4 lg:hidden hover:text-light2'
           onClick={handleClick}
@@ -18,27 +23,27 @@ const Navbar = () => {
           <i className='fas fa-bars'></i>
         </button>
       </div>
-      <ul
-        className={`lg:flex text-center flex-row justify-end ${
-          isOpen ? 'block' : 'hidden'
-        }`}
+      <div
+        className={`lg:flex  text-center flex-row justify-between 
+        `}
       >
-        <li>
+        <div>
+          <img
+            src={logo}
+            alt='logo'
+            className='h-12 w-12 ml-8 mt-2 hidden lg:block'
+          />
+        </div>
+        <div className={`lg:flex ${isOpen ? 'block' : 'hidden'}`}>
           <Link to='about' spy={true} smooth={true} className='nav-btn'>
             About
           </Link>
-        </li>
-        <li>
           <Link to='portfolio' spy={true} smooth={true} className='nav-btn'>
             Portfolio
           </Link>
-        </li>
-        <li>
           <Link to='contact' spy={true} smooth={true} className='nav-btn'>
             Contact
           </Link>
-        </li>
-        <li>
           <a
             href='https://drive.google.com/file/d/1OIxs0nEM963AVNlCMOTS149v2brni6AF/view?usp=sharing'
             className='nav-btn'
@@ -47,8 +52,8 @@ const Navbar = () => {
           >
             Resume
           </a>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
   );
 };
