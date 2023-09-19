2690e0f5d3072303e539711c5f187d86500f4ba0
Kshitij Girkar <girkarkshitij@gmail.com>
2023-09-19 17:03:23 +0530

    add techShop project in portfolio

--- src/components/containers/portfolio/Portfolio.js
+++ src/components/containers/portfolio/Portfolio.js
@@ -1,52 +1,53 @@
-import React from 'react';
-import data from '../../../data';
+import React from "react";
+import data from "../../../data";
 
-import contactProject from '../../../assets/contact-manager.png';
-import githubProject from '../../../assets/github-search-app.png';
-import arProject from '../../../assets/ar-android.png';
+import contactProject from "../../../assets/contact-manager.png";
+import githubProject from "../../../assets/github-search-app.png";
+import arProject from "../../../assets/ar-android.png";
+import techShopProject from "../../../assets/techshop.png";
 
 const Portfolio = () => {
-  const {projects} = data;
+  const { projects } = data;
 
   return (
     <div
-      id='portfolio'
-      className='text-light bg-dark p-8 pt-24 md:px-32 md:pb-32 md:pt-16'
+      id="portfolio"
+      className="text-light bg-dark p-8 pt-24 md:px-32 md:pb-32 md:pt-16"
     >
-      <h1 className='text-3xl h-2/4 text-light text-center pb-4 md:pb-16'>
+      <h1 className="text-3xl h-2/4 text-light text-center pb-4 md:pb-16">
         Portfolio
       </h1>
 
-      <div className='lg:flex pb-20 md:pb-16'>
-        <div className='text-center md:text-left'>
-          <h3 className='md:text-left text-light2 text-3xl pb-4'>
+      <div className="lg:flex pb-20 md:pb-16">
+        <div className="text-center md:text-left">
+          <h3 className="md:text-left text-light2 text-3xl pb-4">
             {projects[0].name}
           </h3>
-          <p className='pb-4 pt-2 text-base md:text-xl text-medium2'>
+          <p className="pb-4 pt-2 text-base md:text-xl text-medium2">
             {projects[0].description}
           </p>
           <a
             href={projects[0].githuburl}
-            className='p-4 pl-0 hover:text-light2'
-            target='_blank'
-            rel='noreferrer'
+            className="p-4 pl-0 hover:text-light2"
+            target="_blank"
+            rel="noreferrer"
           >
-            <i className='fab fa-github'></i>
+            <i className="fab fa-github"></i>
           </a>
           <a
             href={projects[0].siteurl}
-            className='p-4 hover:text-light2'
-            target='_blank'
-            rel='noreferrer'
+            className="p-4 hover:text-light2"
+            target="_blank"
+            rel="noreferrer"
           >
-            <i className='fas fa-external-link-square-alt'></i>
+            <i className="fas fa-external-link-square-alt"></i>
           </a>
-          <ul className='pt-4'>
+          <ul className="pt-4">
             {projects[0].techstack.map((item) => {
               return (
-                <li className='inline text-light2 pr-1 md:pr-3' key={item}>
-                  <i className='fas fa-caret-right'></i>
-                  {' ' + item}
+                <li className="inline text-light2 pr-1 md:pr-3" key={item}>
+                  <i className="fas fa-caret-right"></i>
+                  {" " + item}
                 </li>
               );
             })}
@@ -54,75 +55,75 @@ const Portfolio = () => {
         </div>
         <img
           src={contactProject}
-          className='w-full h-full lg:w-2/4 lg:h-2/4 pt-8 lg:pt-0 lg:ml-8'
-          alt='Contact Manager project screenshot'
+          className="w-full h-full lg:w-2/4 lg:h-2/4 pt-8 lg:pt-0 lg:ml-8"
+          alt="Contact Manager project screenshot"
         />
       </div>
 
-      <div className='flex-row-reverse lg:flex pb-8 md:pb-16'>
-        <div className='text-center md:text-left'>
-          <h3 className='md:text-left text-light2 text-3xl pb-4 lg:pl-8'>
-            {projects[1].name}
+      <div className="flex-row-reverse lg:flex pb-8 md:pb-16">
+        <div className="text-center md:text-left">
+          <h3 className="md:text-left text-light2 text-3xl pb-4 lg:pl-8">
+            {projects[3].name}
           </h3>
-          <p className='pb-4 pt-2 lg:pl-8 text-base md:text-xl text-medium2'>
-            {projects[1].description}
+          <p className="pb-4 pt-2 lg:pl-8 text-base md:text-xl text-medium2">
+            {projects[3].description}
           </p>
           <a
-            href={projects[1].githuburl}
-            className='p-4 lg:pl-8 hover:text-light2'
-            target='_blank'
-            rel='noreferrer'
+            href={projects[3].githuburl}
+            className="p-4 lg:pl-8 hover:text-light2"
+            target="_blank"
+            rel="noreferrer"
           >
-            <i className='fab fa-github'></i>
+            <i className="fab fa-github"></i>
           </a>
           <a
-            href={projects[1].siteurl}
-            className='p-4 hover:text-light2'
-            target='_blank'
-            rel='noreferrer'
+            href={projects[3].siteurl}
+            className="p-4 hover:text-light2"
+            target="_blank"
+            rel="noreferrer"
           >
-            <i className='fas fa-external-link-square-alt'></i>
+            <i className="fas fa-external-link-square-alt"></i>
           </a>
-          <ul className='pt-4 lg:pl-8'>
-            {projects[1].techstack.map((item) => {
+          <ul className="pt-4 lg:pl-8">
+            {projects[3].techstack.map((item) => {
               return (
-                <li className='inline text-light2 pr-1 md:pr-3' key={item}>
-                  <i className='fas fa-caret-right'></i>
-                  {' ' + item}
+                <li className="inline text-light2 pr-1 md:pr-3" key={item}>
+                  <i className="fas fa-caret-right"></i>
+                  {" " + item}
                 </li>
               );
             })}
           </ul>
         </div>
         <img
-          src={githubProject}
-          className='w-full h-full lg:w-2/4 lg:h-2/4 pt-8 lg:pt-0'
-          alt='Github Search App project screenshot'
+          src={techShopProject}
+          className="w-full h-full lg:w-2/4 lg:h-2/4 pt-8 lg:pt-0"
+          alt="TechShop e-commerce project screenshot"
         />
       </div>
 
-      <div className='lg:flex pb-20 md:pb-16'>
-        <div className='text-center md:text-left'>
-          <h3 className='md:text-left text-light2 text-3xl pb-4'>
+      <div className="lg:flex pb-20 md:pb-16">
+        <div className="text-center md:text-left">
+          <h3 className="md:text-left text-light2 text-3xl pb-4">
             {projects[2].name}
           </h3>
-          <p className='pb-4 pt-2 text-base md:text-xl text-medium2'>
+          <p className="pb-4 pt-2 text-base md:text-xl text-medium2">
             {projects[2].description}
           </p>
           <a
             href={projects[2].githuburl}
-            className='p-4 pl-0 hover:text-light2'
-            target='_blank'
-            rel='noreferrer'
+            className="p-4 pl-0 hover:text-light2"
+            target="_blank"
+            rel="noreferrer"
           >
-            <i className='fab fa-github'></i>
+            <i className="fab fa-github"></i>
           </a>
-          <ul className='pt-4'>
+          <ul className="pt-4">
             {projects[2].techstack.map((item) => {
               return (
-                <li className='inline text-light2 pr-1 md:pr-3' key={item}>
-                  <i className='fas fa-caret-right'></i>
-                  {' ' + item}
+                <li className="inline text-light2 pr-1 md:pr-3" key={item}>
+                  <i className="fas fa-caret-right"></i>
+                  {" " + item}
                 </li>
               );
             })}
@@ -130,11 +131,52 @@ const Portfolio = () => {
         </div>
         <img
           src={arProject}
-          className='w-full h-full lg:w-2/4 lg:h-2/4 pt-8 lg:pt-0 lg:ml-8'
-          alt='Learn 3D geometry project screenshot'
+          className="w-full h-full lg:w-2/4 lg:h-2/4 pt-8 lg:pt-0 lg:ml-8"
+          alt="Learn 3D geometry project screenshot"
         />
       </div>
 
+      <div className="flex-row-reverse lg:flex pb-8 md:pb-16">
+        <div className="text-center md:text-left">
+          <h3 className="md:text-left text-light2 text-3xl pb-4 lg:pl-8">
+            {projects[1].name}
+          </h3>
+          <p className="pb-4 pt-2 lg:pl-8 text-base md:text-xl text-medium2">
+            {projects[1].description}
+          </p>
+          <a
+            href={projects[1].githuburl}
+            className="p-4 lg:pl-8 hover:text-light2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+          <a
+            href={projects[1].siteurl}
+            className="p-4 hover:text-light2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fas fa-external-link-square-alt"></i>
+          </a>
+          <ul className="pt-4 lg:pl-8">
+            {projects[1].techstack.map((item) => {
+              return (
+                <li className="inline text-light2 pr-1 md:pr-3" key={item}>
+                  <i className="fas fa-caret-right"></i>
+                  {" " + item}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <img
+          src={githubProject}
+          className="w-full h-full lg:w-2/4 lg:h-2/4 pt-8 lg:pt-0"
+          alt="Github Search App project screenshot"
+        />
+      </div>
     </div>
   );
 };
--- src/data.js
+++ src/data.js
@@ -1,102 +1,113 @@
 const data = {
-  name: 'Kshitij Girkar',
-  role: 'passionate Software Engineer',
+  name: "Kshitij Girkar",
+  role: "passionate Software Engineer",
   socialLinks: [
     {
-      name: 'github',
-      url: 'http://github.com/girkarkshitij',
-      className: 'fab fa-github',
+      name: "github",
+      url: "http://github.com/girkarkshitij",
+      className: "fab fa-github",
     },
     {
-      name: 'linkedin',
-      url: 'https://www.linkedin.com/in/girkarkshitij/',
-      className: 'fab fa-linkedin',
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/girkarkshitij/",
+      className: "fab fa-linkedin",
     },
     {
-      name: 'twitter',
-      url: 'http://twitter.com/girkarkshitij',
-      className: 'fab fa-twitter',
+      name: "twitter",
+      url: "http://twitter.com/girkarkshitij",
+      className: "fab fa-twitter",
     },
     {
-      name: 'instagram',
-      url: 'https://www.instagram.com/kshitij_girkar/',
-      className: 'fab fa-instagram',
+      name: "instagram",
+      url: "https://www.instagram.com/kshitij_girkar/",
+      className: "fab fa-instagram",
     },
   ],
-  address: 'Mumbai, India',
-  website: 'https://kshitijgirkar.me',
+  address: "Mumbai, India",
+  website: "https://kshitijgirkar.me",
   skills: [
     {
       id: 1,
-      skill: 'React.js',
+      skill: "React.js",
     },
     {
       id: 2,
-      skill: 'Node.js',
+      skill: "Node.js",
     },
     {
       id: 3,
-      skill: 'MongoDB',
+      skill: "MongoDB",
     },
     {
       id: 4,
-      skill: 'SQL',
+      skill: "SQL",
     },
     {
       id: 5,
-      skill: 'Git',
+      skill: "Git",
     },
     {
       id: 6,
-      skill: 'Java',
+      skill: "Java",
     },
     {
       id: 7,
-      skill: 'JavaScript',
+      skill: "JavaScript",
     },
   ],
   projects: [
     // Project images in assets folder
     {
-      name: 'Contact Manager',
+      name: "Contact Manager",
       description:
-        'A full-stack contact manager used to store contact details. Front-end is built with React.js and back-end is built using Node.js, Express.js, MongoDB and JWT authentication',
-      githuburl: 'https://github.com/girkarkshitij/contact-manager',
-      siteurl: 'https://contact-manager-mern-qelc.onrender.com/',
+        "A full-stack contact manager used to store contact details. Front-end is built with React.js and back-end is built using Node.js, Express.js, MongoDB and JWT authentication",
+      githuburl: "https://github.com/girkarkshitij/contact-manager",
+      siteurl: "https://contact-manager-mern-qelc.onrender.com/",
       techstack: [
-        'React.js',
-        'Node.js',
-        'Express.js',
-        'MongoDB',
-        'JWT Authentication',
+        "React.js",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "JWT Authentication",
       ],
     },
     {
-      name: 'Github Search App',
+      name: "Github Search App",
       description:
-        'A React.js app to to search Github profiles using Github API. This app uses the Context API along with the React Hooks for state management.',
-      githuburl: 'https://github.com/girkarkshitij/github-search-app',
-      siteurl: 'https://github-search-app-girkarkshitij.netlify.app/',
+        "A React.js app to to search Github profiles using Github API. This app uses the Context API along with the React Hooks for state management.",
+      githuburl: "https://github.com/girkarkshitij/github-search-app",
+      siteurl: "https://github-search-app-girkarkshitij.netlify.app/",
       techstack: [
-        'React.js',
-        'Github API',
-        'React Router',
-        'React Hooks',
-        'Context API',
+        "React.js",
+        "Github API",
+        "React Router",
+        "React Hooks",
+        "Context API",
       ],
     },
     {
-      name: 'Learn 3D Geometry with AR',
-      description: 'An Android app implemented in Kotlin, AR functionality added using ARCore and Sceneform. Used Firebase for backend.',
-      githuburl: 'https://github.com/girkarkshitij/GeometryAR',
-      siteurl: 'https://github.com/girkarkshitij/GeometryAR',
+      name: "Learn 3D Geometry with AR",
+      description:
+        "An Android app implemented in Kotlin, AR functionality added using ARCore and Sceneform. Used Firebase for backend.",
+      githuburl: "https://github.com/girkarkshitij/GeometryAR",
+      siteurl: "https://github.com/girkarkshitij/GeometryAR",
+      techstack: ["Kotlin", "Firebase", "ARCore", "Sceneform"],
+    },
+    {
+      name: "TechShop : e-commerce",
+      description:
+        "A full-stack e-commerce site. Front-end is built with React.js and Redux. Back-end is built using Node.js, Express.js, MongoDB and JWT authentication.",
+      githuburl: "https://github.com/girkarkshitij/e-commerce-mern",
+      siteurl: "https://tech-shop-mern.onrender.com/",
       techstack: [
-        'Kotlin',
-        'Firebase',
-        'ARCore',
-        'Sceneform'
-      ]
-    }
+        "React.js",
+        "Redux",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "JWT Authentication",
+      ],
+    },
   ],
 };
 
